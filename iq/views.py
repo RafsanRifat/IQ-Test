a21eb0d636f83f3ee18962844625681769c2d6d8
Rafsanjani <rafsan.rifat255@gmail.com>
2022-01-09 18:42:10 +0600

    views added for options

--- iq/views.py
+++ iq/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
-from .models import Questions, Question_sets
+from .models import Questions, Question_sets, Options
 
 
 # Create your views here.
@@ -11,10 +11,16 @@ def home(request):
 
 def iq_test(request):
     sets = Question_sets.objects.all()
-    context = {'sets': sets}
+    questions = Questions.objects.all()
+    context = {'sets': sets, 'questions': questions}
     return render(request, 'test.html', context)
 
 
-def iq_api(request):
-    questions = Questions.objects.all()
-    return JsonResponse({"questions": list(questions.values())})
+# def iq_api(request):
+#     questions = Questions.objects.all()
+#     return JsonResponse({"questions": list(questions.values())})
+
+def options(request):
+    options = Options.objects.all()
+    context = {'options': options}
+    return render(request, 'test.html', context)
