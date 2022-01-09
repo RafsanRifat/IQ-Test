bea6ddd801e243af4bc4581c5dabee9b79dcd43d
Rafsanjani <rafsan.rifat255@gmail.com>
2022-01-09 17:43:35 +0600

    migrated and registered

--- iq/admin.py
+++ iq/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Questions, Question_sets
+from .models import Questions, Question_sets, Options
 
 # Register your models here.
 
 admin.site.register(Questions)
 admin.site.register(Question_sets)
+admin.site.register(Options)
